97d93cea3037203da7f25e5138739ad8f118f628
Frederik Zwilling <frederik.zwilling@gmail.com>
2024-12-09 09:58:32 +0100

    some more benchmarking

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use tfhe::prelude::*;
-use tfhe::{generate_keys, set_server_key, ConfigBuilder, FheUint32};
+use tfhe::{generate_keys, set_server_key, ConfigBuilder, FheUint128, FheUint32};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Basic configuration to use homomorphic integers
@@ -10,11 +10,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // On the server side:
     set_server_key(server_keys);
 
-    let user_profile = 0x00ffu32;
+    let user_profile = 0x00ffu128;
     // Encrypting the input data using the (private) client_key
-    let encrypted_user_profile = FheUint32::try_encrypt(user_profile, &client_key)?;
+    let encrypted_user_profile = FheUint128::try_encrypt(user_profile, &client_key)?;
 
-    let target_profile = 0xaaaau32;
+    let target_profile = 0xaaaau128;
 
     let start_time = std::time::Instant::now();
     let encrypted_distance = fhe_hamming_distance(&encrypted_user_profile, target_profile)?;
@@ -27,10 +27,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Overlap Score: {:?}", elapsed);
 
     // Decrypting on the client side:
-    let clear_res: u32 = encrypted_distance.decrypt(&client_key);
+    let clear_res: u128 = encrypted_distance.decrypt(&client_key);
     println!("Distance: {clear_res}");
 
-    let clear_score: u32 = encrypted_score.decrypt(&client_key);
+    let clear_score: u128 = encrypted_score.decrypt(&client_key);
     println!("Score: {clear_score}");
 
     println!("Done!");
@@ -41,14 +41,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
  * Calculate the binary hamming distance between the user profile and the target profile
  */
 fn fhe_hamming_distance(
-    encrypted_user_profile: &FheUint32,
-    target_profile: u32,
+    encrypted_user_profile: &FheUint128,
+    target_profile: u128,
 ) -> Result<FheUint32, Box<dyn std::error::Error>> {
     // Binary XOR
+    let start_time = std::time::Instant::now();
     let encrypted_xor = encrypted_user_profile ^ target_profile;
+    let elapsed = start_time.elapsed();
+    println!("XOR operation: {:?}", elapsed);
 
     // Count set bits to get the distance between both
+    let start_time = std::time::Instant::now();
     let encrypted_distance = encrypted_xor.count_ones();
+    let elapsed = start_time.elapsed();
+    println!("Count ones operation: {:?}", elapsed);
 
     return Ok(encrypted_distance);
 }
@@ -57,14 +63,20 @@ fn fhe_hamming_distance(
  * Calculate the the score of how well the user profile matches the target profile. If it is a perfect match, the score is the amount of bits set in the target profile.
  */
 fn fhe_overlap_score(
-    encrypted_user_profile: &FheUint32,
-    target_profile: u32,
+    encrypted_user_profile: &FheUint128,
+    target_profile: u128,
 ) -> Result<FheUint32, Box<dyn std::error::Error>> {
+    let start_time = std::time::Instant::now();
     // Binary AND
     let encrypted_and = encrypted_user_profile & target_profile;
+    let elapsed = start_time.elapsed();
+    println!("AND operation: {:?}", elapsed);
 
     // Count set bits to get the distance between both
+    let start_time = std::time::Instant::now();
     let encrypted_score = encrypted_and.count_ones();
+    let elapsed = start_time.elapsed();
+    println!("Count ones operation: {:?}", elapsed);
 
     return Ok(encrypted_score);
 }
